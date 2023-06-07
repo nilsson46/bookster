eea1e58a8c9b4d493a4c5f6f075b14b2a29939ca
nilsson46 <nilssonsimon46@gmail.com>
2023-06-07 11:43:03 +0200

    Changed the inputField component

--- vue-project/src/components/InputFieldItem.vue
+++ vue-project/src/components/InputFieldItem.vue
@@ -1,12 +1,41 @@
 <template>
     <div>
-        <input type="text" placeholder="Username...">
-        <input type="password" placeholder="Password...">
+        <label> {{ label }}</label> 
+        <input v-model="value" :type="type" :placeholder="placeholder" @input="handleInput">
         
     </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        value: {
+            required: true,
+        }, 
+        label: {
+            required: true,
+        },
+        type: {
+            default: "text",
+        },
+        placeholder: {
+            default: "",
+        },
+    },
+    methods:{
+        handleInput(event: Event) {
+            this.$emit("input", (event.target as HTMLInputElement).value);
+        },
+    },
+});
+</script>
+
 <style scoped>
+    label {
+        margin-right: 1rem;
+    }
     input {
         margin-top: 1rem;
         margin-right: 1rem;
